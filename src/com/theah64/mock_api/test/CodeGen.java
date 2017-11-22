d4aad54a930a612289ff6b737083dd72874af768
theapache64-charlie <theapache64@gmail.com>
2017-11-22 16:44:46 +0530

    Repairing model 1

---
+++ src/com/theah64/mock_api/test/CodeGen.java
@@ -0,0 +1,7 @@
+package com.theah64.mock_api.test;
+
+/**
+ * Created by theapache64 on 22/11/17.
+ */
+public class CodeGen {
+}
--- src/com/theah64/mock_api/test/MyClass.java
+++ src/com/theah64/mock_api/test/MyClass.java
@@ -1,25 +1,79 @@
 package com.theah64.mock_api.test;
 
-import java.util.Random;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 public class MyClass {
 
-
     public static void main(String[] args) {
-        System.out.println(generateRandomWords(4));
+
+        //Reading sample file
+        final File sampleFile = new File(System.getProperty("user.dir") + File.separator + "sample.json");
+        final StringBuilder jsonData = new StringBuilder();
+        try {
+            final BufferedReader br = new BufferedReader(new FileReader(sampleFile));
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonData.append(line).append("\n");
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final JSONObject joResponse = new JSONObject(jsonData);
+        final String pojoCode = CodeGen.getCode(joResponse);
+        System.out.println(pojoCode);
     }
 
-    public static String generateRandomWords(int numberOfWords) {
-        final StringBuilder stringBuilder = new StringBuilder();
-        Random random = new Random();
-        for (int i = 0; i < numberOfWords; i++) {
-            char[] word = new char[random.nextInt(8) + 3]; // words of length 3 through 10. (1 and 2 letter words are boring.)
-            for (int j = 0; j < word.length; j++) {
-                word[j] = (char) ('a' + random.nextInt(26));
-            }
-            stringBuilder.append(word).append(" ");
+    static class CodeGen {
+
+        public static String getCode(JSONObject joResponse) {
+
+            final List<Class> classes = getClasses(joResponse);
+
+            return null;
         }
-        return stringBuilder.toString();
+
+        private static List<Class> getClasses(JSONObject joResponse) {
+
+            return null;
+        }
     }
+
+
+    static class Class {
+
+        private final List<Variable> variables;
+
+        Class(List<Variable> variables) {
+            this.variables = variables;
+        }
+
+    }
+
+    static class Variable {
+
+        private final String dataType, variableName;
+
+        Variable(String dataType, String variableName) {
+            this.dataType = dataType;
+            this.variableName = variableName;
+        }
+
+        public String getDataType() {
+            return dataType;
+        }
+
+        public String getVariableName() {
+            return variableName;
+        }
+    }
+
 
 }
