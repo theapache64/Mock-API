b129aef1d62e99afabbcffef4d3648f041b68ee5
theapache64-charlie <theapache64@gmail.com>
2018-01-04 22:50:53 +0530

    CodeGen paused

--- Sample.java
+++ Sample.java
@@ -6,4 +6,17 @@ import com.google.gson.annotations.SerializedName;
 
 public class Sample {
 
-}
+	@SerializedName("error")
+	private final boolean error;
+
+	@SerializedName("message")
+	private final String message;
+
+	@SerializedName("data")
+	private final Data data;
+
+	public Sample(boolean error,String message,Data data){
+		this.error = error;
+		this.message = message;
+		this.data = data;
+	}
--- src/com/theah64/mock_api/lab/CodeGen.java
+++ src/com/theah64/mock_api/lab/CodeGen.java
@@ -7,20 +7,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class CodeGen {
 
     public static void main(String[] args) {
 
         final StringBuilder jsonString = new StringBuilder();
         final File jsonFile = new File("sample.json");
         final String mainClassName = "Sample";
+        boolean isRetrofitModel = true;
 
         final List<Class.Variable> variables = new ArrayList<>();
         variables.add(new Class.Variable("error", "boolean"));
         variables.add(new Class.Variable("message", "String"));
         variables.add(new Class.Variable("data", "Data"));
 
-        final Class c = new Class(mainClassName, null, variables);
+        final Class mainClass = new Class(mainClassName, null, variables);
         final StringBuilder pojoBuilder = new StringBuilder();
 
         //Package name
@@ -32,10 +33,43 @@ public class Main {
         //Signature
         pojoBuilder.append("//Developed using mock api\n\n");
 
-        //Main class signature
-        pojoBuilder.append("public class ").append(mainClassName).append(" {");
+        //CodeGen class signature
+        pojoBuilder.append("public class ").append(mainClass.getName()).append(" {\n\n");
+
+        final StringBuilder constructorVariables = new StringBuilder();
+        final StringBuilder constructorInitializer = new StringBuilder();
+        final StringBuilder getterBuilder = new StringBuilder();
+
+        //Building variables
+        for (final Class.Variable variable : variables) {
+
+            if (isRetrofitModel) {
+                //Retrofit model support
+                pojoBuilder.append(String.format("\t@SerializedName(\"%s\")\n", variable.getName()));
+            }
+
+            constructorVariables.append(String.format("%s %s,", variable.getDataType(), variable.getName()));
+            constructorInitializer.append(String.format("\n\t\tthis.%s = %s;", variable.getName(), variable.getName()));
 
 
+            //Declaration
+            pojoBuilder.append(String.format("\tprivate final %s %s;\n\n", variable.getDataType(), variable.getName()));
+
+
+        }
+
+
+        //CodeGen class constructor
+        pojoBuilder
+                .append("\tpublic ")
+                .append(mainClass.getName())
+                .append("(")
+                .append(constructorVariables.substring(0, constructorVariables.length() - 1))
+                .append("){")
+                .append(constructorInitializer).append("\n\t}");
+
+
+        System.out.println(pojoBuilder);
         save(pojoBuilder.toString());
     }
 
@@ -70,6 +104,10 @@ public class Main {
 
         public List<Variable> getVariables() {
             return variables;
+        }
+
+        public String getName() {
+            return name;
         }
 
         static class Variable {
