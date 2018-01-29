dca9dcea004f7527d9a5d8b579e39d2bf03bc3f6
theapache64-alpha <theapache64@gmail.com>
2018-01-29 14:53:08 +0530

    .backup

--- src/com/theah64/mock_api/utils/ActivityCodeGen.java
+++ src/com/theah64/mock_api/utils/ActivityCodeGen.java
@@ -1,8 +1,16 @@
 package com.theah64.mock_api.utils;
 
+import com.theah64.mock_api.models.Project;
+
 public class ActivityCodeGen {
     public static final String KEY_ROUTE_ID = "route_id";
     public static final String KEY_API_KEY = "api_key";
+
+    public static ActivityCode generate(String apiKey, String routeId) {
+
+
+        return null;
+    }
 
 
     public class ActivityCode {
